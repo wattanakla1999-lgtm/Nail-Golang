23b9e0fb4f2765b8c79ccb348b4c8a39f9306a9e
Wattana <wattana.kla.1999@gmail.com>
2026-07-12 19:31:47 +0700

    refactor database connection to use new postgres.Config structure

--- internal/database/db.go
+++ internal/database/db.go
@@ -9,7 +9,10 @@ import (
 )
 
 func Connect(dsn string) *gorm.DB {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
